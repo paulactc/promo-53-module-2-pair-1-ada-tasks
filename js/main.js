151bbd8c482eb79b8b489c22fbdfb2bf27a5c92d
Paula Castillo <paula_ctc@hotmail.es>
2025-05-09 12:17:37 +0200

    add fetch in JS

---
+++ js/main.js
@@ -0,0 +1,57 @@
+'use strict';
+
+let tasks =[];
+
+/*= [
+  { name: "Recoger setas en el campo", completed: true, id: 1 },
+  { name: "Comprar pilas", completed: true, id: 2 },
+  { name: "Poner una lavadora de blancos", completed: true, id: 3 },
+  {
+    name: "Aprender cómo se realizan las peticiones al servidor en JavaScript",
+    completed: false,
+    id: 4,
+  },
+];
+
+/*const renderTasks = (tasks) => {
+const html = `<li> $ (tasks) </li>`; 
+return html;
+}
+
+
+let html = ""; 
+
+
+
+console.log (tasksUl);
+
+
+for (const task of tasks) {
+  tasksUl.innerHTML += `<li>${task.name}</li>`;
+  
+}*/
+
+//DOCUMENT QUERY SELECTOR
+
+const tasksUl=document.querySelector(".js-task-list");
+
+//VARIABLES DE DATOS 
+
+const GITHUB_USER = "<paulactc>";
+const SERVER_URL = `https://dev.adalab.es/api/todo/${GITHUB_USER}`;
+
+//FETCH
+fetch(SERVER_URL)
+.then( (response)=>{
+  return response.json();})
+.then((data)=>
+  {tasks=data;
+for (const task of tasks) {
+  tasksUl.innerHTML += `<li>${task}</li>`;
+}
+
+});
+
+
+//Completa el código;
+//Guarda la respuesta obtenida enla variable para el listado de tareas: `tasks`
