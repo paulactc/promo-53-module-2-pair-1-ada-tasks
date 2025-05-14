316f8928ebdf9e793bcaf72adcc2f6f2b3bd9b7e
Paula Castillo <paula_ctc@hotmail.es>
2025-05-14 11:04:40 +0200

    add fetch method post

--- js/main.js
+++ js/main.js
@@ -32,6 +32,10 @@ for (const task of tasks) {
   
 }*/
 
+
+const tasksLocalStorage = JSON.parse(localStorage.getItem("tasks"));
+console.log(tasksLocalStorage);
+
 //DOCUMENT QUERY SELECTOR
 
 const tasksUl=document.querySelector(".js-task-list");
@@ -46,7 +50,7 @@ const SERVER_URL = `https://dev.adalab.es/api/todo/${GITHUB_USER}`;
 //VARIABLE ARRAY
 let tasks =[];
 
-//FETCH
+//FETCH GET
 fetch(SERVER_URL)
 .then((response) =>
   response.json())
@@ -54,13 +58,13 @@ fetch(SERVER_URL)
   tasks=data.results 
   
   for (const task of tasks) {
-  tasksUl.innerHTML += `<li>${task.name}</li>`;
+  tasksUl.innerHTML += `<li>${task.name} </li>`;
   console.log(task.name);
 }
 
 });
 
-//POST
+//FUNCION MANEJADORA
 const handleNewTask = (event) => {
   event.preventDefault();
 
@@ -72,6 +76,9 @@ const handleNewTask = (event) => {
     desc: taskDesc,
   };
 
+
+    //enviar info
+    // FETCH POST
    fetch(
     `https://dev.adalab.es/api/todo/${GITHUB_USER}`,
     {
@@ -83,16 +90,39 @@ const handleNewTask = (event) => {
     .then(res => res.json())
     .then(dataResponseSave => {
       console.log(dataResponseSave);
-      
-    });
-  
+      tasks.push(newTask);
+
+      tasksUl.innerHTML="";
+
+       for (const task of tasks) {
+  tasksUl.innerHTML += `<li>${task.name} </li>`;
+  console.log(task.name);
 }
-  // 4. Vuelve a pintar las tareas
-;
 
+    });
+      }
 
+      //EVENTO DE ESCUCHA 
+     saveBtn.addEventListener('click',handleNewTask) 
+     
+      //CONDICIONAL SERVIDOR
 
+     if (tasksLocalStorage !== null) {
+       tasksUl = JSON.parse(localStorage.getItem('kittens-cache'));
+  // si (existe el listado de tareas en Local Storage)
+  // pinta la lista de tareas almacenadas en tasksLocalStorage
+} else {
+  //sino existe el listado de tareas en el local storage
+  // pide los datos al servidor
+  fetch(SERVER_URL)
+    .then((response) => response.json())
+    .then((data) => {
+      //guarda el listado obtenido en el Local Storage
+      // pinta la lista de tareas
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+}
+   
 
-
-//Completa el código;
-//Guarda la respuesta obtenida enla variable para el listado de tareas: `tasks`
